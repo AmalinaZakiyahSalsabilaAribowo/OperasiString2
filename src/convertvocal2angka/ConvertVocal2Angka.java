c66034a5dee188255cf6eb05a2c4fac81ce5bd6e
ASUS <ASUS@LAPTOP-929I2GH2>
2018-08-16 20:15:23 +0700

    Tambah Input

--- src/convertvocal2angka/ConvertVocal2Angka.java
+++ src/convertvocal2angka/ConvertVocal2Angka.java
@@ -1,5 +1,6 @@
 package convertvocal2angka;
 import java.util.Scanner;
+import java.util.Locale;
 public class ConvertVocal2Angka {
 
     private static void tampilJudul (String identitas){
@@ -12,7 +13,19 @@ public class ConvertVocal2Angka {
         String Identitas = "Amalina Zakiyah Salsabila Aribowo / XRPL 2 / 04";
         
         tampilJudul (Identitas);
-            
+        
+        
+        
     }
-    
+            private static String tampilInput() {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("Masukkan Kalimat: ");
+        String kalimat = scanner.nextLine();
+        System.out.println("Kalimat Asli : " + kalimat);
+        
+        return kalimat;
+    }
+    String kalimat = tampilInput();
+
 }
