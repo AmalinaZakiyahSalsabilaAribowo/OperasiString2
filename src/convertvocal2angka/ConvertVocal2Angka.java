4ec7e1548ebe998c168d292769a440af1281b110
ASUS <ASUS@LAPTOP-929I2GH2>
2018-08-16 19:13:39 +0700

    Initial Commit

---
+++ src/convertvocal2angka/ConvertVocal2Angka.java
@@ -0,0 +1,18 @@
+package convertvocal2angka;
+import java.util.Scanner;
+public class ConvertVocal2Angka {
+
+    private static void tampilJudul (String identitas){
+        System.out.println("Identitas : "+ identitas);
+        
+        System.out.println("\nConvert Kalimat Alay Angka (Vokal Ke Angka)\n");
+        
+        
+        
+        String Identitas = "Amalina Zakiyah Salsabila Aribowo / XRPL 2 / 04";
+        
+        tampilJudul (Identitas);
+            
+    }
+    
+}
