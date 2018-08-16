43e79f807e2a43e5bf52fb2557d8d53205c74971
ASUS <ASUS@LAPTOP-929I2GH2>
2018-08-16 20:31:56 +0700

    Tambah Tampil Per Kata

--- src/convertvocal2angka/ConvertVocal2Angka.java
+++ src/convertvocal2angka/ConvertVocal2Angka.java
@@ -40,5 +40,18 @@ private static String vocal2Angka(String kalimat){
         kalimat = kalimat.replace(arConvert[i][0], arConvert[i][1]);
                 
     return kalimat;    
+    
+    
+    
 }
+    private static void tampilPerKata(String kalimat, String convert){
+    String[] arrKal = kalimat.split(" ");
+    String[] arrCon = convert.split(" ");
+                    
+    for(int i=0;i<arrKal.length;i++)
+    System.out.println(arrKal[i]+" => "+arrCon[i]);              
+                    
+    tampilPerKata(kalimat, convert);
+}
+
 }
